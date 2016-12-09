571c2bc8f81857ba53ed0b2a2a787d65016b205d
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-09 14:36:04 -0800

    add views to add and see stylists

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,9 +11,20 @@ namespace Salon
       Get["/"] = _ =>{
         return View["index.cshtml"];
       };
-      Get["stylists"]= _ =>{
+      Get["/stylists"]= _ =>{
         List<Stylist> allStylists = Stylist.GetAll();
         return View["stylists.cshtml", allStylists];
+      };
+      Get["/stylists/new"] = _ =>{
+        return View["stylists_add.cshtml"];
+      };
+      Get["/clients/new"]= _ =>{
+        return View["clients_add.cshtml"];
+      };
+      Post["/stylists/new"]= _ =>{
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        return View["success.cshtml"];
       };
     }
   }
