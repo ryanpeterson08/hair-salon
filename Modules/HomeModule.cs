22b3703e90fe8a107cbf958a0adf4659df3298e5
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-09 15:44:22 -0800

    add views to update stylists

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -26,6 +26,14 @@ namespace Salon
         var SelectedStylist = Stylist.Find(parameters.id);
         return View["stylist.cshtml", SelectedStylist];
       };
+      Get["stylist/edit/{id}"] = parameters => {
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        return View["stylist_edit.cshtml", SelectedStylist];
+      };
+      Get["/client-edit/{id}"] = parameters => {
+        Client SelectedClient = Client.Find(parameters.id);
+        return View["client_edit.cshtml", SelectedClient];
+      };
       Post["/stylists/new"]= _ =>{
         Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
         newStylist.Save();
@@ -35,6 +43,16 @@ namespace Salon
         Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
         newClient.Save();
         return View["success.cshtml"];
+      };
+      Patch["/stylist/edit/{id}"] = parameters => {
+      Stylist SelectedStylist = Stylist.Find(parameters.id);
+      SelectedStylist.Update(Request.Form["stylist-name"]);
+      return View["success.cshtml"];
+      };
+      Patch["/client-edit/{id}"] = parameters => {
+      Client SelectedClient = Client.Find(parameters.id);
+      SelectedClient.Update(Request.Form["client-name"]);
+      return View["success.cshtml"];
       };
     }
   }
