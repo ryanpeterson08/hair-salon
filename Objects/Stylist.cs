91b535fa078eb10de4c7c53af11d3fdd7c192c39
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-09 10:05:38 -0800

    add Save function that saves stylist to db

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -39,5 +39,33 @@ namespace Salon
     {
       return _name;
     }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name) OUPUT INSERTED.id VALUES (@StylistName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StylistName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
   }
 }
